0973c8241c63af26776308959f470e3336f72f60
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2024-08-20 03:10:49 +0900

    feat: integration graphqlmodule and build first graphql query api

--- tartib.ts
+++ tartib.ts
@@ -1,5 +1,8 @@
 // 1 dars
 // npm i @nestja/config ikkita portni ishlatishgaimkon beradi
+// loyihamizda GraphQL integratsiyasini amalga oshiramiz va ilk GraohQL API requestimizni amalga oshiramiz
+// loyihani standartlarini shakllantiramiz
+//
   
 
 
