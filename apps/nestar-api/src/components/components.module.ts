404a37d2cead8d4eabf93da35ee01b1b0575e5c2
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2024-08-22 19:29:29 +0900

    feat: create components related modules

--- apps/nestar-api/src/components/components.module.ts
+++ apps/nestar-api/src/components/components.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PropertyModule } from './property/property.module';
 import { DatabaseModule } from './database/database.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-  imports: [MemberModule, PropertyModule, DatabaseModule]
+  imports: [MemberModule, PropertyModule, DatabaseModule, CommentModule, LikeModule, ViewModule, FollowModule, BoardArticleModule]
 })
 export class ComponentsModule {}
