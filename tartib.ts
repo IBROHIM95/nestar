2d13be04c2f283602be60ced8c28ebfc1b617b80
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2024-08-22 14:21:02 +0900

    feat: connecting MongoDB

--- tartib.ts
+++ tartib.ts
@@ -1,8 +1,10 @@
-// 1 dars
+// 
 // npm i @nestja/config ikkita portni ishlatishgaimkon beradi
 // loyihamizda GraphQL integratsiyasini amalga oshiramiz va ilk GraohQL API requestimizni amalga oshiramiz
 // loyihani standartlarini shakllantiramiz
-//
+// database hosil qilamiz
+// loyihani databasega ulaymiz
+// loyihani barcha schema modellarini tashkillaymiz va enumlarni hosil qilamiz 
   
 
 
