8ffd403d03604c9e42dc172ef6732c8f3188c72f
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2024-08-27 17:20:06 +0900

    feat: develop logging via global interceptors

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
   /*biz bu yerda global integratsiani amalga oshirdik
   yani  butun tizimimiz uchun  ishlaydi degani 
   */
   const app = await NestFactory.create(AppModule); //NestFactory orqali NestJS ilovasi yaratilinadi
+  app.useGlobalInterceptors(new LoggingInterceptor())
   app.useGlobalPipes(new ValidationPipe()) //pipeni global tarsda ishlatish
   await app.listen(process.env.PORT_API ?? 3000);
 }
