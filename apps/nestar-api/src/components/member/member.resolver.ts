4afc4804b76c69aa00552906ed76c1317db6b9ec
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2024-09-01 16:41:51 +0900

    feat: view system integration

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -12,6 +12,7 @@ import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { shapeIntoMongoDBObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 
 @Resolver()     //controll va rooterlar o'rnida ishlaydi va GarphQL APIlarni qurib beradi
@@ -61,13 +62,13 @@ export class MemberResolver {
     
    }
 
-   
+   @UseGuards(WithoutGuard)
    @Query(() => Member)
-   public async getMember(@Args('memberId') input: string): Promise<Member> {
+   public async getMember(@Args('memberId') input: string, @AuthMember('_id') memberId: ObjectId) : Promise<Member> {
     console.log('Query getMember');
     const targetId = shapeIntoMongoDBObjectId(input)
 
-    return this.memberService.getMember(targetId)  
+    return this.memberService.getMember(memberId, targetId)  
    }
 
    /* ADMIN */
