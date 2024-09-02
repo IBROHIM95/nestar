c838de5e01dc0c44b69d8eb19bd4ab14c2aeb6f2
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2024-09-02 19:33:21 +0900

    fix: modify upload images configrations

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
+import {graphqlUploadExpress} from 'graphql-upload'
+import  * as express from 'express'
 
 async function bootstrap() {
   /*biz bu yerda global integratsiani amalga oshirdik
@@ -10,6 +12,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule); //NestFactory orqali NestJS ilovasi yaratilinadi
   app.useGlobalInterceptors(new LoggingInterceptor())
   app.useGlobalPipes(new ValidationPipe()) //pipeni global tarsda ishlatish
+  app.enableCors({origin: true, credentials: true})
+
+  app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10}))
+  app.use('/uploads', express.static('./uploads')) //faylni tashqaolamga ochish
+  
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
